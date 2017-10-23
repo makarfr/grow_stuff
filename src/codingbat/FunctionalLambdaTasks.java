35cf40c7253a87d6a33412dc248bc39cb02a8474
Olga_Kovalenko1 <makarfr@gmail.com>
2017-10-23 15:44:20 +0300

    codingbat functional-2

---
+++ src/codingbat/FunctionalLambdaTasks.java
@@ -0,0 +1,36 @@
+package codingbat;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class FunctionalLambdaTasks {
+
+    public static void main(String[] args) {
+        FunctionalLambdaTasks lambdaTasks = new FunctionalLambdaTasks();
+
+        System.out.println(lambdaTasks.doubling(Arrays.asList(1, 2, 3)));
+        ArrayList<Integer> integers = new ArrayList(Arrays.asList(9,12, 29, 3));
+        System.out.println(lambdaTasks.no9(integers));
+        System.out.println(lambdaTasks.noTeen(integers));
+    }
+
+    public List<Integer> doubling(List<Integer> nums) {
+        return nums.stream().map(s -> s*2).collect(Collectors.toList());
+    }
+
+    public List<String> addStar(List<String> strings) {
+        return strings.stream().map(s -> s+ "*").collect(Collectors.toList());
+    }
+    public List<Integer> no9(List<Integer> nums) {
+        nums.removeIf(s -> (s%10) == 9);
+        return  nums;
+    }
+
+    public List<Integer> noTeen(List<Integer> nums) {
+        nums.removeIf(s -> (s >= 13) &&(s <= 19));
+        return nums;
+    }
+
+}
